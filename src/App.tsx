2d406bdee187c297a79dd92e92c49d3a97d54412
Elizabeth Koch <elk677@g.harvard.edu>
2024-09-29 22:07:39 -0400

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import colors from './colors.json';
+import colors from './colors.json'; // Import colors from colors.json
 
 const App = () => {
     return (
@@ -12,14 +12,17 @@ const App = () => {
                             backgroundColor: `#${color.hex}`,
                             color: '#fff',
                             margin: '10px',
-                            padding: '20px',
+                            padding: '10px',
                             borderRadius: '5px',
-                            width: '150px',
+                            width: '150px',  // Fixed width
+                            height: '150px',  // Fixed height
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
                             textAlign: 'center',
                         }}
                     >
                         <p>{color.name}</p>
-                        <p>#{color.hex}</p>
                     </div>
                 ))}
             </div>
